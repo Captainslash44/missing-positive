7651a66c6e8e9948ca61e2588a39eaf75ea61176
Halim Njeim <michaelnjeim44@gmail.com>
2024-11-14 19:32:29 +0200

    fixed the code after reviewing it in the session with Charbel

--- missing-positive-function.py
+++ missing-positive-function.py
@@ -2,13 +2,16 @@ def First_missing_positive(numbers):
 
     maximum = max(numbers) # Find the maximum number in the array, O(length)
     missing_number = -1 # Initiating the missing number to a negative value
-
+    MISSING_NUMBERS = []
     for i in range(maximum):
         if (maximum - i) not in numbers: # We check for every value between 0 and our maximum number
             missing_number = maximum - i
+            MISSING_NUMBERS.append(missing_number)
 
     if missing_number == -1: # If all values between our maximum and 0 exist, then the missing number is larger by 1.
         missing_number = maximum + 1
+    else:
+        missing_number = max(MISSING_NUMBERS)
 
     return missing_number
 
